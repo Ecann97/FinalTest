a5197a0096afde02c38b3f94ba7d6ded6012db93
Evan Cann <sEvanCann@yccc.edu>
2018-12-12 18:56:55 -0500

    second commit

--- Interface.java
+++ Interface.java
@@ -12,19 +12,20 @@ import javax.swing.Box;
 public class Interface {
 
 	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
-	private JTextField textField_7;
-	private JTextField textField_8;
-	private JTextField textField_9;
-	private JTextField textField_10;
+	private JTextField c1;
+	private JTextField c2;
+	private JTextField c3;
+	private JTextField e1;
+	private JTextField e2;
+	private JTextField e3;
+	private JTextField upperRange;
+	private JTextField lowerRange;
+	private JTextField d1;
+	private JTextField d2;
+	private JTextField d3;
 	private JLabel lblIntegral;
-
+	
+	static System s = new System();
 	/**
 	 * Launch the application.
 	 */
@@ -61,7 +62,7 @@ public class Interface {
 		btnSubmit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
-			lblIntegral.setText(String.valueOf(1));
+			lblIntegral.setText(String.valueOf(s.integrand(c1.getText(), c2.getText(), c3.getText(), e1.getText(), e2.getText(), e3.getText(), d1.getText(), d2.getText(), d3.getText(), upperRange.getText(), lowerRange.getText())));
 			
 			}
 		});
@@ -71,18 +72,18 @@ public class Interface {
 		JButton btnReset = new JButton("Reset");
 		btnReset.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				textField.setText("");
-				textField_1.setText("");
-				textField_2.setText("");
-				textField_3.setText("");
-				textField_4.setText("");
-				textField_5.setText("");
-				textField_6.setText("");
-				textField_7.setText("");
-				textField_8.setText("");
-				textField_9.setText("");
-				textField_10.setText("");
-				
+				c1.setText("");
+				c2.setText("");
+				c3.setText("");
+				e1.setText("");
+				e2.setText("");
+				e3.setText("");
+				upperRange.setText("");
+				lowerRange.setText("");
+				d1.setText("");
+				d2.setText("");
+				d3.setText("");
+				lblIntegral.setText("");
 			}
 		});
 		btnReset.setBounds(335, 11, 89, 23);
@@ -100,20 +101,20 @@ public class Interface {
 		label_1.setBounds(231, 86, 29, 14);
 		frame.getContentPane().add(label_1);
 		
-		textField = new JTextField();
-		textField.setBounds(39, 83, 22, 20);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		c1 = new JTextField();
+		c1.setBounds(39, 83, 22, 20);
+		frame.getContentPane().add(c1);
+		c1.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(121, 83, 22, 20);
-		frame.getContentPane().add(textField_1);
+		c2 = new JTextField();
+		c2.setColumns(10);
+		c2.setBounds(121, 83, 22, 20);
+		frame.getContentPane().add(c2);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(199, 83, 22, 20);
-		frame.getContentPane().add(textField_2);
+		c3 = new JTextField();
+		c3.setColumns(10);
+		c3.setBounds(199, 83, 22, 20);
+		frame.getContentPane().add(c3);
 		
 		JLabel label_2 = new JLabel("+");
 		label_2.setBounds(96, 82, 8, 23);
@@ -123,20 +124,20 @@ public class Interface {
 		label_3.setBounds(174, 82, 8, 23);
 		frame.getContentPane().add(label_3);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(77, 66, 22, 20);
-		frame.getContentPane().add(textField_3);
-		textField_3.setColumns(10);
+		e1 = new JTextField();
+		e1.setBounds(77, 66, 22, 20);
+		frame.getContentPane().add(e1);
+		e1.setColumns(10);
 		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		textField_4.setBounds(160, 66, 22, 20);
-		frame.getContentPane().add(textField_4);
+		e2 = new JTextField();
+		e2.setColumns(10);
+		e2.setBounds(160, 66, 22, 20);
+		frame.getContentPane().add(e2);
 		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		textField_5.setBounds(231, 66, 22, 20);
-		frame.getContentPane().add(textField_5);
+		e3 = new JTextField();
+		e3.setColumns(10);
+		e3.setBounds(231, 66, 22, 20);
+		frame.getContentPane().add(e3);
 		
 		JLabel lblUpperRange = new JLabel("Upper Range");
 		lblUpperRange.setBounds(10, 179, 89, 14);
@@ -146,30 +147,30 @@ public class Interface {
 		lblLowerRange.setBounds(10, 204, 89, 14);
 		frame.getContentPane().add(lblLowerRange);
 		
-		textField_6 = new JTextField();
-		textField_6.setBounds(121, 176, 86, 20);
-		frame.getContentPane().add(textField_6);
-		textField_6.setColumns(10);
+		upperRange = new JTextField();
+		upperRange.setBounds(121, 176, 86, 20);
+		frame.getContentPane().add(upperRange);
+		upperRange.setColumns(10);
 		
-		textField_7 = new JTextField();
-		textField_7.setColumns(10);
-		textField_7.setBounds(121, 201, 86, 20);
-		frame.getContentPane().add(textField_7);
+		lowerRange = new JTextField();
+		lowerRange.setColumns(10);
+		lowerRange.setBounds(121, 201, 86, 20);
+		frame.getContentPane().add(lowerRange);
 		
-		textField_8 = new JTextField();
-		textField_8.setBounds(57, 110, 29, 20);
-		frame.getContentPane().add(textField_8);
-		textField_8.setColumns(10);
+		d1 = new JTextField();
+		d1.setBounds(57, 110, 29, 20);
+		frame.getContentPane().add(d1);
+		d1.setColumns(10);
 		
-		textField_9 = new JTextField();
-		textField_9.setColumns(10);
-		textField_9.setBounds(141, 111, 29, 20);
-		frame.getContentPane().add(textField_9);
+		d2 = new JTextField();
+		d2.setColumns(10);
+		d2.setBounds(141, 111, 29, 20);
+		frame.getContentPane().add(d2);
 		
-		textField_10 = new JTextField();
-		textField_10.setColumns(10);
-		textField_10.setBounds(224, 111, 29, 20);
-		frame.getContentPane().add(textField_10);
+		d3 = new JTextField();
+		d3.setColumns(10);
+		d3.setBounds(224, 111, 29, 20);
+		frame.getContentPane().add(d3);
 		
 		lblIntegral = new JLabel("");
 		lblIntegral.setBounds(320, 179, 46, 14);
--- System.java
+++ System.java
@@ -1,125 +1,33 @@
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JTextPane;
-import javax.swing.Box;
 
 public class System {
 	
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
-	private JTextField textField_7;
-	private JTextField textField_8;
-	private JTextField textField_9;
-	private JTextField textField_10;
-	private JLabel lblIntegral;
 	
-	public JTextField getTextField() {
-		return textField;
-	}
-	public void setTextField(JTextField textField) {
-		this.textField = textField;
-	}
-	public JTextField getTextField_1() {
-		return textField_1;
-	}
-	public void setTextField_1(JTextField textField_1) {
-		this.textField_1 = textField_1;
-	}
-	public JTextField getTextField_2() {
-		return textField_2;
-	}
-	public void setTextField_2(JTextField textField_2) {
-		this.textField_2 = textField_2;
-	}
-	public JTextField getTextField_3() {
-		return textField_3;
-	}
-	public void setTextField_3(JTextField textField_3) {
-		this.textField_3 = textField_3;
-	}
-	public JTextField getTextField_4() {
-		return textField_4;
-	}
-	public void setTextField_4(JTextField textField_4) {
-		this.textField_4 = textField_4;
-	}
-	public JTextField getTextField_5() {
-		return textField_5;
-	}
-	public void setTextField_5(JTextField textField_5) {
-		this.textField_5 = textField_5;
-	}
-	public JTextField getTextField_6() {
-		return textField_6;
-	}
-	public void setTextField_6(JTextField textField_6) {
-		this.textField_6 = textField_6;
-	}
-	public JTextField getTextField_7() {
-		return textField_7;
-	}
-	public void setTextField_7(JTextField textField_7) {
-		this.textField_7 = textField_7;
-	}
-	public JTextField getTextField_8() {
-		return textField_8;
-	}
-	public void setTextField_8(JTextField textField_8) {
-		this.textField_8 = textField_8;
-	}
-	public JTextField getTextField_9() {
-		return textField_9;
-	}
-	public void setTextField_9(JTextField textField_9) {
-		this.textField_9 = textField_9;
-	}
-	public JTextField getTextField_10() {
-		return textField_10;
-	}
-	public void setTextField_10(JTextField textField_10) {
-		this.textField_10 = textField_10;
-	}
-	public JLabel getLblIntegral() {
-		return lblIntegral;
-	}
-	public void setLblIntegral(JLabel lblIntegral) {
-		this.lblIntegral = lblIntegral;
-	}
 
-	public double integrand()
+	public double integrand(String c1, String c2, String c3, String e1, String e2, String e3, String d1, String d2, String d3, String upperRange, String lowerRange)
 	{
-		int a = Integer.getInteger(textField.getText()).intValue(); //c
-		int d = Integer.getInteger(textField_3.getText()).intValue(); //e
-		int i = Integer.getInteger(textField_8.getText()).intValue(); //d
+		int a = Integer.getInteger(c1).intValue(); //c
+		int d = Integer.getInteger(e1).intValue(); //e
+		int i = Integer.getInteger(d1).intValue(); //d
 		
-		int b = Integer.getInteger(textField_1.getText()).intValue(); //c
-		int e = Integer.getInteger(textField_4.getText()).intValue(); //e
-		int j = Integer.getInteger(textField_9.getText()).intValue(); //d
+		int b = Integer.getInteger(c2).intValue(); //c
+		int e = Integer.getInteger(e2).intValue(); //e
+		int j = Integer.getInteger(d2).intValue(); //d
 		
-		int c = Integer.getInteger(textField_2.getText()).intValue(); //c
-		int f = Integer.getInteger(textField_5.getText()).intValue(); //e
-		int k = Integer.getInteger(textField_10.getText()).intValue(); //d
+		int c = Integer.getInteger(c3).intValue(); //c
+		int f = Integer.getInteger(e3).intValue(); //e
+		int k = Integer.getInteger(d3).intValue(); //d
 		
-		int g = Integer.getInteger(textField_6.getText()).intValue(); //u
-		int h = Integer.getInteger(textField_7.getText()).intValue(); //l
-		
-		
+		int g = Integer.getInteger(upperRange).intValue(); //u
+		int h = Integer.getInteger(lowerRange).intValue(); //l
 		
 		
 		
-		double y = ( ((a*(g^(d+1)))/(i*(d+1)))+((b*(g^(e+1)))/(j*(e+1)))+((c*(g^(f+1)))/(k*(f+1))) );
-		double t = ( ((a*(h^(d+1)))/(i*(d+1)))+((b*(h^(e+1)))/(j*(e+1)))+((c*(h^(f+1)))/(k*(f+1))) );
+		
+		
+		double y = ((a*(g^(d+1)))/(i*(d+1)))  +  ((b*(g^(e+1)))/(j*(e+1)))  + ((c*(g^(f+1)))/(k*(f+1)));
+		double t = ((a*(h^(d+1)))/(i*(d+1))) +   ((b*(h^(e+1)))/(j*(e+1)))  + ((c*(h^(f+1)))/(k*(f+1)));
 		
 		double u = y - t;
 		
